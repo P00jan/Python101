101b969c79dd3de08d1c4b8f65fb4f61b8b23350
Poojan Shah <95199585+P00jan@users.noreply.github.com>
2023-10-10 13:45:37 -0400

    strings.py

--- Basic-concepts/Strings.py
+++ Basic-concepts/Strings.py
@@ -12,21 +12,36 @@ print(len(message))
 
 # Some Strings methods
 # Remember Strings location starts from 0
+
 # You are printing the character which is located in 1st place
 print(message[1])
+
 # You are printing the character starting from 0th location till 1st location but not including the 1st character
 print(message[:1])
+
 # You are printing the character which is in last third place of whole string
 print(message[-3])
+
 # From 0th location till last location but not including it.
 print(message[:-1])
+
 # from 0th location till 5th location and not including it.
 print(message[0:5])
 
-""" 
-Outputs: o
-G
-i
-Good Mornin
-Good 
-"""
+# For lower case characters
+print(message.lower())
+
+# For upper case characters
+print(message.upper())
+
+# for replacing a word
+new = (message.replace("Morning", "night"))
+print(new)
+
+# For concatenating
+greeting = "How are you?"
+name = "Poojan"
+print(greeting + " " + name)
+
+# f strings
+print(f"{greeting}" + " "+f"{name}")
