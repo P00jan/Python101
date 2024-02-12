700dcc01fec7b0321a1d08f2503d2c7367bad548
Poojan Shah <95199585+P00jan@users.noreply.github.com>
2024-02-12 15:32:02 -0500

    Update

---
+++ Basic-concepts/basic.py
@@ -0,0 +1,5 @@
+"""Let's learn the basics of Python\n"""
+"""
+What is Print and why is it used?\n
+Print is used to show the output to the screen. Anything to display on the screen will be done by Print Funtion\n
+"""
