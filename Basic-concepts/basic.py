b14264e1ace0cc0abf711dc747cab6ed11e8c85d
Poojan Shah <95199585+P00jan@users.noreply.github.com>
2024-02-12 15:47:46 -0500

    Update

--- Basic-concepts/basic.py
+++ Basic-concepts/basic.py
@@ -3,3 +3,23 @@
 What is Print and why is it used?
 Print is used to show the output to the screen. Anything to display on the screen will be done by Print Funtion
 """
+print("Hello, Pythooooon!!!!")
+
+print("02101010")  # numbers or letters must be in " " format in order to be printed
+
+print("This is how you print in \n different lines")
+
+print()  # use this to print an empty line
+
+print("My \n" "Name \n" "is Pythooooon")
+
+"""Variables"""
+"""What are Variables? Variable are used to define something. For example A = "Python"
+Here we define a variable 'A' and assign it a value Python, variables make things easy when you are doing coding."""
+
+a = ("Hello")
+b = ("Python")
+print(a + " " + b)
+
+"""Here we define a variable 'a' & 'b' with values then we print those 2 variables with '+' sign in between which 
+is nothing but the concatenation of two strings"""
