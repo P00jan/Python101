1ccf6a879dfdbba7f44d4fbfafd4c78bc92683cf
Poojan Shah <95199585+P00jan@users.noreply.github.com>
2024-02-12 22:11:44 -0500

    Update

--- Basic-concepts/basic.py
+++ Basic-concepts/basic.py
@@ -23,3 +23,53 @@ print(a + " " + b)
 
 """Here we define a variable 'a' & 'b' with values then we print those 2 variables with '+' sign in between which 
 is nothing but the concatenation of two strings"""
+
+"""Operators and Data Types"""
+"""Data Types is nothing but collection of certain groups. For example Alphabets or words are called Strings"""
+
+a = "Hello, Python"  # String
+b = 12  # integer
+c = 2.5  # Float (Any number with decimal point is called Float)
+d = True  # Boolean
+e = False  # Boolean
+
+"""Python has 4 types of collection data types"""
+
+f = {"name": "Python",
+     "age": 1991,
+     "Useful": True}
+"""This is called dictionary data type, where you have key:value format. In this case we have name,age and useful as keys
+   and respective values to them. Dictionary is a collection which is ordered and changeable. No duplicate members
+   Example use cases: representing a mapping of user IDs to usernames, storing configuration settings,
+   caching data with keys for quick retrieval."""
+
+
+g = ["python", "Java", 1921, 1990, True]
+"""This is called List data type, which typically works as an array in Python. 
+   List is a collection which is ordered and changeable. Allows duplicate members.
+   Example use cases: storing a list of tasks, maintaining a history of actions, managing a sequence of elements."""
+
+h = ("Python", "Java", 1990, 1992, False)
+"""This is Tuple. Tuple is a collection which is ordered and unchangeable. Allows duplicate members. 
+   Example use cases: returning multiple values from a function, representing fixed collections like RGB colors, 
+   representing coordinates in a grid."""
+
+i = {"Python", "Java", 1990, 1992, False}
+"""This is set data type. Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
+    Example use cases: removing duplicates from a list, checking membership efficiently, 
+    performing set operations like intersection or difference."""
+
+""" Use lists when you need an ordered collection that may change.
+    Use tuples when you need an ordered collection that should not change.
+    Use dictionaries when you need to associate unique keys with values.
+    Use sets when you need an unordered collection of unique items or need to perform set operations."""
+
+print(a, "\n",
+      b, "\n",
+      c, "\n",
+      d, "\n",
+      e, "\n",
+      f, "\n",
+      g, "\n",
+      h, "\n",
+      i)
